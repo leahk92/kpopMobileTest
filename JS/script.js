ef6e0938e34ad652a4bb2272e3d0c8796e77a18d
leahk92 <leahkang92@gmail.com>
2025-02-28 15:53:19 +0900

    test01

---
+++ JS/script.js
@@ -0,0 +1,40 @@
+// main
+$('.nationBox').slick({
+   dots: false,
+   infinite: true,
+   speed: 500,
+   autoplay: true,
+   autoplaySpeed: 5000,
+   slidesToShow: 7,
+   //fade: true,
+   adaptiveHeight: true,
+   responsive: [
+      {
+         breakpoint: 2560,
+         settings: {
+            slidesToShow: 7,
+         },
+         breakpoint: 1024,
+         settings: {
+            slidesToShow: 5,
+         },
+      },
+   ],
+});
+
+// 반응형 내비탭
+var menuLeft = document.getElementById('cbp-spmenu-s1'),
+   showLeft = document.getElementById('showLeft'),
+   body = document.body;
+
+showLeft.onclick = function () {
+   classie.toggle(this, 'active');
+   classie.toggle(menuLeft, 'cbp-spmenu-open');
+   disableOther('showLeft');
+};
+
+function disableOther(button) {
+   if (button !== 'showLeft') {
+      classie.toggle(showLeft, 'disabled');
+   }
+}
